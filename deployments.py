0b140da46c5f050cfcdf979b20f4f41420a50011
katie <katie924p@gmail.com>
2025-04-24 09:57:24 +0800

    deployments.py

--- deployments.py
+++ deployments.py
@@ -5,6 +5,7 @@ from flows.flow2 import hello_flow as another_flow
 
 
 if __name__ == "__main__":
-    hello_deploy = hello_flow.to_deployment(name="Hello deploy")
-    another_deploy = another_flow.to_deployment(name="Another deploy")
-    serve(hello_deploy, another_deploy)
+    serve(
+        hello_flow.to_deployment(name="Hello deploy"),
+        another_flow.to_deployment(name="Another deploy")
+    )
